197b3e390766db5634032fb049ecf960ee45185d
Yousuf-Gazi <gaziyousufridoy@gmail.com>
2025-10-24 10:34:11 +0600

    feat: python codes in set directory is done

---
+++ python_code_practice/set/add_and_remove.py
@@ -0,0 +1,8 @@
+# Demonstrate how to add and remove elements in a set.
+numbers = {1, 2, 3}
+numbers.add(4)
+numbers.discard(2)
+numbers.update([5, 6])
+
+if __name__ == '__main__':
+    print("Updated set:", numbers)
---
+++ python_code_practice/set/set_from_string.py
@@ -0,0 +1,6 @@
+# Convert strings to sets to find unique characters
+word = "banana"
+unique_letters = set(word)
+
+if __name__ == '__main__':
+    print("Unique letters in 'banana':", unique_letters)
